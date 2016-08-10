8156b944cf5308d368726d20e35f3feddd5f2cb3
Jose Bolivar <josevalle.info@gmail.com>
2016-08-10 09:04:51 +0000

    descarga de EasyAdminBundle, actualizacion de los ficheros Appkernel.php,routing.yml,config.yml,composer.json,composer.lock

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new AppBundle\AppBundle(),
             
             new FOS\UserBundle\FOSUserBundle(),
